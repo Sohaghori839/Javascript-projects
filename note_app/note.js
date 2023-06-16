a0c18b942199f229a617f9bf0c1ec356108b7e0d
Sohaghori839 <sohaghori839@gmail.com>
2023-06-16 16:47:04 +0500

    note app

--- note_app/note.js
+++ note_app/note.js
@@ -1,5 +1,6 @@
 const addBox = document.querySelector(".add-box");
 popupBox = document.querySelector(".popup-box");
+popupTitle = popupBox.querySelector("header p");
 closeIcon = popupBox.querySelector("header i");
 titleTag = popupBox.querySelector("input");
 descTag = popupBox.querySelector("textarea");
@@ -10,19 +11,25 @@ const months = ["January", "Feburary", "Mearch", "April", "May", "June", "July",
 // getting localstorage notes if exist and parsing them
 // to js object else passing a empty array to notes
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
+let isUpdate = false, updateId;
 addBox.addEventListener("click", () => {
+    titleTag.focus();
     popupBox.classList.add("show")
 });
 
 closeIcon.addEventListener("click", () => {
-    popupBox.classList.remove("show")
+   
+    isUpdate = false;
     titleTag.value = "";
     descTag.value = "";
+    addBtn.innerText = "Add a Note";
+    addTitle.innerText = "Add a new Note";
+    popupBox.classList.remove("show");
 });
 
 function showNotes() {
     document.querySelectorAll(".note").forEach(note => note.remove());
-    notes.forEach(note => {
+    notes.forEach((note, index) => {
         let liTag = `<li class="note">
        <div class="details">
            <p>${note.title}</p>
@@ -32,16 +39,45 @@ function showNotes() {
            <span>${note.date}</span>
            <div class="settings">
              <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+             <ul class="menu">
+             <li onclick= "updateNote(${index}, '${note.title}','${note.description}')"><i class="uil uil-pen"></i>Edit</li>
+             <li onclick="deleteNote(${index})"><i class="uil uil-trash"></i>Delete</li>
+            </ul> 
+    
            </div>
        </div>
    </li>`;
-   addBox.insertAdjacentHTML("afterend",liTag);
+        addBox.insertAdjacentHTML("afterend", liTag);
     });
 }
 showNotes();
 
-function showMenu(elem){
-    console.log(elem.parentElement.add("show"));
+function showMenu(elem) {
+    elem.parentElement.classList.add("show");
+    document.addEventListener("click", e => {
+        // removing show class from the settings menu on document click
+        if (e.target != "I" || e.target != elem) {
+            elem.parentElement.classList.remove("show");
+        }
+    });
+}
+function deleteNote(noteId){
+    let confirm = confirm("Are you sure you went to delete this note?")
+    if(confirmDel) return;
+ notes.splice(noteId,1); // remove selected note from array/tasks
+ localStorage.setItem("notes", JSON.stringify(notes));
+ showNotes();
+}
+
+function updateNote(noteId, title, desc) {
+    isUpdate = true;
+    updateId =noteId;
+    addBox.click();
+    titleTag.value = title;
+    descTag.value = desc;
+    addBtn.innerText = "Update Note";
+    addTitle.innerText = "Update Note";
+    console.log(noteId, title, desc);
 }
 addBtn.addEventListener("click", e => {
     e.preventDefault();
@@ -56,6 +92,13 @@ addBtn.addEventListener("click", e => {
         let noteInfo = {
             title: noteTitle, description: noteDesc,
             date: `${month} ${day}, ${year}`
+        }
+        if (!isUpdate) {
+            notes.push(noteInfo);//adding new note a notes
+        }
+        else{
+            isUpdate = false;
+            notes[updateId] = noteInfo; //updating spacified note
         }
         notes.push(noteInfo)//adding new note to notes
         // saving notes to local storage
